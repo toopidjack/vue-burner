0bf3cdab9860671a90f978daa28bd674850c83b8
Jack Greene <Jack.Greene@jacobs.com>
2024-03-13 22:52:38 -0600

    working quiz app

---
+++ hello-world/src/components/HeaderItem.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="header-item">
+        <b-nav tabs>
+            <b-nav-item disabled><b>Fancy Quiz App</b></b-nav-item>
+            <b-nav-item disabled>Counter {{ numCorrect }} / {{ numTotal }}</b-nav-item>
+        </b-nav>
+    </div>
+</template>
+
+<script>
+export default {
+    props: [
+        'numCorrect',
+        'numTotal'
+    ]
+}
+
+</script>
